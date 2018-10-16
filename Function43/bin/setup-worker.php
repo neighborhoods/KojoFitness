3c0240c5551db6af84c184bfe3302ea55bee3faa
p-mucha <przemyslaw.mucha@55places.com>
2018-10-16 17:16:49 -0500

    KOJO-29 | Example DLC with max iterations

---
+++ Function43/bin/setup-worker.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\Finder\Finder;
+use Neighborhoods\Kojo\Api\V1\Job;
+
+$discoverableDirectories[] = __DIR__ . '/../src/V1/Environment';
+$finder = new Finder();
+$finder->name('*.yml');
+$finder->files()->in($discoverableDirectories);
+$jobCreator = (new Job\Type\Service())->addYmlServiceFinder($finder)->getNewJobTypeRegistrar();
+$jobCreator->setCode(Neighborhoods\KojoFitnessFunction43\V1\Worker::JOB_TYPE_CODE)
+    ->setWorkerClassUri(\Neighborhoods\KojoFitnessFunction43\V1\Worker\Facade::class)
+    ->setWorkerMethod('start')
+    ->setName('Capped-Iteration Protean DLCP Example')
+    ->setCronExpression('* * * * *')
+    ->setCanWorkInParallel(true)
+    ->setDefaultImportance(10)
+    ->setScheduleLimit(50)
+    ->setScheduleLimitAllowance(1)
+    ->setIsEnabled(true)
+    ->setAutoCompleteSuccess(false)
+    ->setAutoDeleteIntervalDuration('PT0S');
+$jobCreator->save();
