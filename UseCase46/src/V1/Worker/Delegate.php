3cfe59b592d4534d1887ec1cda7c72d9818d74de
Alex Berryman <alex.berryman@55places.com>
2019-03-29 14:30:36 -0500

    fluentd-local-Elast-Kibana | increase the number of non-exceptions

--- UseCase46/src/V1/Worker/Delegate.php
+++ UseCase46/src/V1/Worker/Delegate.php
@@ -11,16 +11,25 @@ class Delegate implements DelegateInterface
 
     public function businessLogic() : DelegateInterface
     {
-        if ((bool)random_int(0, 1)) {
-            $this->getV1WorkerQueueMessage()->delete();
-        } else {
-            $this->onlyTakesIntegers('Not very lucky');
-        }
+        $i = rand(1, 100);
+        switch (true) {
+            case  (1 <= $i) && ($i <= 50):
+                $this->getV1WorkerQueueMessage()->delete();
+                break;
+            case (51 <= $i) && ($i <= 70):
+                $this->onlyTakesIntegers('Not very lucky');
+                break;
+            case (71 <= $i) && ($i <= 100):
+                throw new \LogicException('That want very logical');
+            default:
+                throw new \UnexpectedValueException('Do arrays start at 1?');
 
+        }
         return $this;
     }
 
-    public function onlyTakesIntegers(int $number){
+    public function onlyTakesIntegers(int $number)
+    {
         return $number;
     }
 }
