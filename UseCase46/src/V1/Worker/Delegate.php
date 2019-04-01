a2bfbc8cce4e263b0db4de756e7804c784004471
Alex Berryman <alex.berryman@55places.com>
2019-04-01 17:16:05 -0500

    fluentd-local-Elast-Kibana | Deep chain of previous errors that will cause line splitting on fluentd

--- UseCase46/src/V1/Worker/Delegate.php
+++ UseCase46/src/V1/Worker/Delegate.php
@@ -13,9 +13,9 @@ class Delegate implements DelegateInterface
 
     public function businessLogic() : DelegateInterface
     {
-        $i = random_int(0, 100);
+        $i = random_int(1, 100);
         switch (true) {
-            case  (1 <= $i) && ($i <= 80):
+            case  (1 <= $i) && ($i <= 60):
                 $context = [
                     'event_type' => 'task_status',
                     'top_level_status' => 'success',
@@ -29,11 +29,14 @@ class Delegate implements DelegateInterface
                 $this->getV1WorkerQueueMessage()->delete();
                 $this->getApiV1WorkerService()->getLogger()->alert('Deleted the message boss!', $context);
                 break;
-            case (81 <= $i) && ($i <= 90):
+            case (61 <= $i) && ($i <= 80):
                 $this->onlyTakesIntegers('Not very lucky');
                 break;
-            case (91 <= $i) && ($i <= 100):
+            case (81 <= $i) && ($i <= 95):
                 throw new \LogicException('That was not very logical');
+                break;
+            case (95 <= $i) && ($i <= 100):
+                $this->createDeepPreviousExceptionChain();
                 break;
             default:
                 throw new \UnexpectedValueException('Do arrays start at 1?');
@@ -62,5 +65,70 @@ class Delegate implements DelegateInterface
     protected function randomElementFromArray(array $seed)
     {
         return $seed[random_int(0, count($seed) - 1)];
+    }
+
+    protected function createDeepPreviousExceptionChain()
+    {
+        try {
+            try {
+                try {
+                    try {
+                        try {
+                            try {
+                                try {
+                                    try {
+                                        try {
+                                            try {
+                                                try {
+                                                    try {
+                                                        try {
+                                                            throw new \BadMethodCallException('root cause');
+                                                        } catch (\BadMethodCallException $exception) {
+                                                            throw new \OverflowException('Deeper', 1, $exception);
+                                                        }
+                                                    } catch (\OverflowException $exception) {
+                                                        throw new \ArithmeticError('The Kick', 2, $exception);
+                                                    }
+                                                } catch (\ArithmeticError $exception) {
+                                                    throw new \OutOfBoundsException(
+                                                        'This is not the last one',
+                                                        3,
+                                                        $exception
+                                                    );
+                                                }
+                                            } catch (\ArithmeticError $exception) {
+                                                throw new \OutOfBoundsException(
+                                                    'This is not the last one',
+                                                    3,
+                                                    $exception
+                                                );
+                                            }
+                                        } catch (\OutOfBoundsException $exception) {
+                                            throw new \OutOfBoundsException('This is not the last one', 4, $exception);
+                                        }
+                                    } catch (\OutOfBoundsException $exception) {
+                                        throw new \OutOfBoundsException('This is not the last one', 5, $exception);
+                                    }
+                                } catch (\OutOfBoundsException $exception) {
+                                    throw new \OutOfBoundsException('This is not the last one', 6, $exception);
+                                }
+                            } catch (\OutOfBoundsException $exception) {
+                                throw new \OutOfBoundsException('This is not the last one', 7, $exception);
+                            }
+                        } catch (\OutOfBoundsException $exception) {
+                            throw new \OutOfBoundsException('This is not the last one', 8, $exception);
+                        }
+                    } catch (\OutOfBoundsException $exception) {
+                        throw new \OutOfBoundsException('This is not the last one', 9, $exception);
+                    }
+                } catch (\OutOfBoundsException $exception) {
+                    throw new \OutOfBoundsException('This is not the last one', 10, $exception);
+                }
+            } catch (\OutOfBoundsException $exception) {
+                throw new \OutOfBoundsException('This is not the last one', 11, $exception);
+            }
+        } catch (\OutOfBoundsException $exception) {
+            throw new \OutOfBoundsException('This is the last one', 12, $exception);
+        }
     }
 }
